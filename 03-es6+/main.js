f99f6eba98e31e3547ffbbab254cdaae363ee89b
Guram Abramishvili <guramabramishvili0@gmail.com>
2023-12-10 03:11:18 +0400

    03-ES6+

--- 03-es6+/main.js
+++ 03-es6+/main.js
@@ -8,6 +8,31 @@ function sumAndMultiplication(num1, num2, ...args) {
   return [sum, multiplication];
 }
 
-function returnCity() {}
+// prettier-ignore
+function returnCity({banks: [,,{address: { city }}]}) {
+  return city;
+}
+
+function deepCopy(object) {
+  if (object === null || typeof object !== "object") {
+    return object;
+  }
+
+  let newObject = { ...object };
+
+  for (const key in newObject) {
+    if (newObject.hasOwnProperty(key)) {
+      newObject[key] = deepCopy(newObject[key]);
+    }
+  }
+  return newObject;
+}
+
+let user = { banks: ["tbc", "bog", { address: { city: "Rustavi" } }] };
 console.log(sumAndMultiplication(2, "samiani"));
 console.log(sumAndMultiplication(2, 3, 5, 8));
+console.log(returnCity(user));
+
+const clonedObject = deepCopy(user);
+clonedObject.banks[2].address.city = "Tbilisi";
+console.log(clonedObject.banks[2].address.city === user.banks[2].address.city);
