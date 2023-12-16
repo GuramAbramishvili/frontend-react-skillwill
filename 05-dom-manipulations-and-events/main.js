0283948f9adb5580097711f58595978ebf06c34b
Guram Abramishvili <guramabramishvili0@gmail.com>
2023-12-16 23:54:24 +0400

    05-dom-manipulations-and-events

---
+++ 05-dom-manipulations-and-events/main.js
@@ -0,0 +1,5 @@
+const button1 = document.createElement("button");
+const div1 = document.createElement("div");
+div1.innerText = "Click button to hide me";
+button.setAttribute("id", "button1Id");
+document.body.append(div1, button1);
